ce87ee7d67693952da6f0801ad3d27bdf2cfe924
Bunei shadieh <buneishadrack5@gmail.com>
2021-04-11 11:52:58 +0300

    added file

--- neighborhood_watch/settings.py
+++ neighborhood_watch/settings.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -84,6 +85,7 @@ DATABASES = {
         'USER': 'bunei',
         'PASSWORD': 'bunei',
     }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
