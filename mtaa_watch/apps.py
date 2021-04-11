64a886558b9a1116a05f42a006594e4264495540
Bunei shadieh <buneishadrack5@gmail.com>
2021-04-11 22:36:07 +0300

    added media

--- mtaa_watch/apps.py
+++ mtaa_watch/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class MtaaWatchConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
+    default_auto_field = 'users.apps.UsersConfig'
     name = 'mtaa_watch'
