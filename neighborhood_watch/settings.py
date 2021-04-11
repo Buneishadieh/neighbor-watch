58135b24ab52f5c5310586e27a58804d6788c929
Bunei shadieh <buneishadrack5@gmail.com>
2021-04-11 13:30:58 +0300

    added users

--- neighborhood_watch/settings.py
+++ neighborhood_watch/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'bootstrap4',
     'mtaa_watch',
+    'users'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
